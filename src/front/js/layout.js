ba276f0cdac34af6b86607649e306d8940b41362
bertablancpastor <72865106+bertablancpastor@users.noreply.github.com>
2023-08-07 11:44:30 +0000

    last commit

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -32,7 +32,7 @@ const Layout = () => {
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Login />} path="/login" />
                         <Route element={<Signup />} path="/signup" />
-                        {!localStorage.getItem("token") ? <Route element={<h1>Not found!</h1>}/> : <Route element={<Profile />} path="/profile" />}
+                        {localStorage.getItem("token") && <Route element={<Profile />} path="/profile" />}
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
